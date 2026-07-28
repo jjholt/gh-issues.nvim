114cfde2268e66f70df1f470937ec1ee8909495d
JJ <jonathanjamesholt@gmail.com>
2026-07-28 22:58:48 +0100

    added automatic binding for PR and issue calls

--- lua/gh-issues/init.lua
+++ lua/gh-issues/init.lua
@@ -1,7 +1,7 @@
 ---@class gh-issues.Keybinds
 ---@field issues string
 ---@field pull_request string
----@field clean_cache string
+---@field clear_markers string
 ---@field add_to_quickfix string
 ---@field nav_review_comments string[]
 
@@ -20,7 +20,7 @@ local default_config = {
     keybinds = {
         issues = "<leader>gi",
         pull_request = "<leader>gpr",
-        clean_cache = "<leader>gc",
+        clear_markers = "<leader>gc",
         add_to_quickfix = "<C-a>",
         nav_review_comments = {"]c", "[c"},
     },
@@ -33,6 +33,12 @@ M.config = default_config
 ---@param opts? gh-issues.Config
 M.setup = function(opts)
     M.config = vim.tbl_deep_extend("force", default_config, opts or {})
+
+    local kb = M.config.keybinds
+    local api = require("gh-issues.api")
+    vim.keymap.set("n", kb.issues, api.open_issues, {desc = "Populate issues into quickfix list"})
+    vim.keymap.set("n", kb.pull_request, api.open_pull_request, {desc = "Populate pull requests into quickfix list"})
+    vim.keymap.set("n", kb.clear_markers, api.clear_markers, {desc = "Clear all diagnostic markers from source"})
 end
 
 return M
